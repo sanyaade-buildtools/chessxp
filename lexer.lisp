c807398e29476095938628a6f66bb3bc07c27c9b
unknown <jeff.massung@VOLI31236.thqinc.com>
2012-09-26 12:19:45 -0500

    More work on the lexer.

--- lexer.lisp
+++ lexer.lisp
@@ -5,6 +5,10 @@
   (:use :cl)
   (:nicknames :lex)
   (:export
+   #:compile-re
+   #:match-re
+   #:find-re
+   #:split-re
    #:deflexer
    #:parse))
 
@@ -16,10 +20,8 @@
   (:documentation "Regular expression."))
 
 (defclass lex-state ()
-  ((source             :initarg :source :accessor lex-source)
-   (pos    :initform 0 :initarg :pos    :accessor lex-pos)
-   (line   :initform 1 :initarg :line   :accessor lex-line)
-   (token              :initarg :token  :accessor lex-token))
+  ((source   :initarg :source   :accessor lex-source)
+   (captures :initarg :captures :accessor lex-captures))
   (:documentation "Token pattern match."))
 
 (defmethod print-object ((re re) s)
@@ -31,25 +33,46 @@
   "Output the result of a match."
   (print-unreadable-object (lex s :type t :identity t)))
 
-(defun parse (source p)
-  "Parse a string with a parse combinator. Return the token parsed or nil."
-  (with-output-to-string (s)
-    (let ((st (make-instance 'lex-state :source source :token s)))
-      (unless (funcall p st)
-        (return-from parse nil)))))
+(defun match-re (re s &key (start 0) (end (length s)) exact)
+  "Check to see if a regexp pattern matches a string."
+  (with-input-from-string (source s :start start :end end)
+    (let ((st (make-instance 'lex-state :source source)))
+      (when (funcall (re-expression re) st)
+        (if exact
+            (when (= (lex-pos st) (length s))
+              (values s (lex-pos st)))
+          (values (subseq s start (lex-pos st)) (lex-pos st))))))
+
+(defun find-re (re s &key (start 0) (end (length s)) all)
+  "Find a regexp pattern match somewhere in a string."
+  (do ((st (make-instance 'lex-state :source s :pos start :end end)
+           (make-instance 'lex-state :source s :pos start :end end)))
+      ((funcall (re-expression re) st)
+       (let ((m (cons start (lex-pos st))))
+         (if (not all)
+             m
+           (cons m (find-re re s :start (lex-pos st) :end end :all t)))))
+    (unless (< (incf start) (length s))
+      (return nil))))
+
+(defun split-re (re s &key (start 0) (end (length s)) all coalesce-seps)
+  "Split a string into one or more strings by regexp pattern match."
+  (let ((m (find-re re s :start start :end end :all all)))
+    (if (null m)
+        (subseq s start end)
+      (if (null all)
+          (destructuring-bind (left right)
+              m
+            (values (subseq s (car left) (cdr left))
+                    (subseq s (car right) (cdr right))))
+        (mapcar #'(lambda (m) (subseq s (car m) (cdr m))) m)))))
 
 (defun next (st pred)
   "Read the next character, update the pos, test against predicate."
-  (with-slots (source pos line token)
+  (with-slots (source pos end)
       st
-    (let ((c (char source pos)))
-      (when (funcall pred c)
-        (prog1
-            c
-          (princ c token)
-          (incf pos)
-          (when (char= c #\newline)
-            (incf line)))))))
+    (when (and (< pos end) (funcall pred (char source pos)))
+      (incf pos))))
 
 (defun bind (&rest ps)
   "Bind parse combinators together to compose a new combinator."
@@ -66,46 +89,38 @@
 (defun either (p1 p2)
   "Try one parse combinator, if it fails, try another."
   #'(lambda (st)
-      (with-slots (pos token)
-          st
-        (let ((old-pos pos)
-              (token-pos (file-position token)))
-          (or (funcall p1 st)
-              (progn
-                (setf pos old-pos)
-                (file-position token token-pos)
-                (funcall p2 st)))))))
+      (let ((pos (lex-pos st)))
+        (or (funcall p1 st)
+            (progn
+              (setf (lex-pos st) pos)
+              (funcall p2 st))))))
 
 (defun any-char (&optional match-newline-p)
   "Expect a non-eoi character."
-  (flet ((pred (x)
-           (and x (if match-newline-p t (char/= x #\newline)))))
-    #'(lambda (st)
-        (next st #'pred))))
+  #'(lambda (st)
+      (next st #'(lambda (x)
+                   (and x (if match-newline-p t (char/= x #\newline)))))))
 
 (defun ch (c &optional case-fold)
   "Match an exact character."
-  (flet ((pred (x)
-           (let ((test (if case-fold #'char-equal #'char=)))
-             (and x (funcall test x c)))))
+  (let ((test (if case-fold #'char-equal #'char=)))
     #'(lambda (st)
-        (next st #'pred))))
+        (next st #'(lambda (x)
+                     (and x (funcall test x c)))))))
 
 (defun one-of (cs &optional case-fold)
   "Match any character from a set."
-  (flet ((pred (x)
-           (let ((test (if case-fold #'char-equal #'char=)))
-             (and x (find x cs :test test)))))
+  (let ((test (if case-fold #'char-equal #'char=)))
     #'(lambda (st)
-        (next st #'pred))))
+        (next st #'(lambda (x)
+                     (and x (find x cs :test test)))))))
 
 (defun none-of (cs &optional case-fold)
   "Match any character not in a set."
-  (flet ((pred (x)
-           (let ((test (if case-fold #'char-equal #'char=)))
-             (and x (not (find x cs :test test))))))
+  (let ((test (if case-fold #'char-equal #'char=)))
     #'(lambda (st)
-        (next st #'pred))))
+        (next st #'(lambda (x)
+                     (and x (not (find x cs :test test))))))))
 
 (defun many (p)
   "Match a parse combinator zero or more times."
@@ -118,7 +133,11 @@
 
 (defun many-til (p term)
   "Match a parse combinator many times until a terminal."
-  (either term (bind p (many-til p term))))
+  #'(lambda (st)
+      (do ()
+          ((funcall term st) t)
+        (unless (funcall p st)
+          (return nil)))))
 
 (defun between (b1 b2 &optional match-newline-p)
   "Match everything between two characters."
